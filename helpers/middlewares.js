fea7f4fa71d0ea1a49f5ace029140dc0890c5fad
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-11-14 12:36:40 -0800

    update checking for answers, questions objects for survey

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -8,47 +8,61 @@ function mustBeInteger(req, res, next) {
         next()
 }
 
-function isEmpty(obj) {
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key))
-          return false
-    }
+function isValidObject(obj, res) {
+    /* Checking if its an object type  */
+    if (!(obj && typeof(obj) === 'object')) 
+        res.status(400).json({message: 'Each item in the survey needs to be an object type' })
+
+    /* Check for key length to be 2 */   
+    if (Object.keys(obj).length !== 2)
+        res.status(400).json({message: 'You need to pass two keys into each object <question> and <answer>'})
+ 
+    /* Check for only question and answer keys in object */
+    if (!('question' in obj) || !('answer' in obj))
+        res.status(400).json({message: 'There should be a "question" or "answer" key in each survey object'})
+   
+    /* Check if question length is longer than 10 and answer length should be 0 */ 
+    if (obj.question instanceof String || obj.question.length < 10)
+        res.status(400).json({message: 'A valid question should be a string type and longer than 10 character'})
+    if (obj.answer instanceof String || obj.answer.length !== 0)
+        res.status(400).json({message: 'When creating a survey, the answer should be a string type and be blank. Ex:{"question":"Why do you consume cannabis?", "answer": ""}'})
     return true
 }
 
-function notCorrectLength(obj) {
-    if (Object.keys(obj).length !== 2)
-        return true
-    return false
+
+function checkParameters(req, res) {
+    const { title, tags, survey } = req.body
+    if (!(title && tags && survey))
+        res.status(400).json({ message: 'Need to have a dictionary with keys => title, tags and survey' })
+    return true
 }
+
+function tagSurveyIsArray(req, res) {
+    /* checks for tags and survey to both be of an array type, survey should also not be empty */
+    const { title, tags, survey } = req.body
+    if (!Array.isArray(tags))
+        res.status(400).json({ message: 'The key "tags" needs to have a value of an array type'})
+    if (!Array.isArray(survey))
+        res.status(400).json({message: 'The key "survey" needs to have a value of an array with object type items'})
+    if (Array.isArray(survey) && survey.length === 0)
+        res.status(400).json({message: 'They survey value should not be an empty array'})
+    return true
+}
+
+
+
 function checkFieldsPost(req, res, next) {
     const { title, tags, survey } = req.body
 
-    console.log(req.body)
-
-    all_checks = []
- 
-    if (!(title && tags && survey))
-        res.status(400).json({ message: 'Need to have a dictionary with keys => title, tags and survey' })
-    else if (!Array.isArray(tags))
-        res.status(400).json({ message: 'The key "tags" needs to have a value of an array type'})
-    else if (!Array.isArray(survey))
-        res.status(400).json({message: 'The key "survey" needs to have a value of an array with object type items'})
-    else if (Array.isArray(survey))
-        if (survey.length === 0)
-            res.status(400).json({message: 'They survey value should not be an empty array'})
-        for (let i = 0; i < survey.length; i++)
-            /* Checking if its an object type */
-            if (!(survey[i] && typeof(survey[i]) === 'object')) 
-                res.status(400).json({message: 'Each item in the survey needs to be a object type' })
-            /* Checking if an object is empty */
-            //if (isEmpty(survey[i]))
-                //res.status(400).json({message: 'You cannot pass an empty object as an item in the Survey'})
-            //if (notCorrectLength(survey[i]))
-                //res.status(400).json({message: 'Each object in the survey has to have a length of 2'}) 
-            /* Checking if each survey object has the key question */ 
-    else 
-        next()
+    checks = [checkParameters, tagSurveyIsArray]
+    
+    for (let i = 0; i < checks.length; i++)
+        checks[i](req, res)
+    
+    for (let i = 0; i < survey.length; i++)
+        isValidObject(survey[i], res) 
+    
+    next()
 }
 
 module.exports = {
